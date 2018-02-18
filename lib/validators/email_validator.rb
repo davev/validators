4489ab7dd598fc3ee64194ae3dbb4284b9d3f92b
Dave VanderKloot <dave@1530design.com>
2018-02-18 16:26:14 -0600

    update email regex to disallow ending with a dot

--- lib/validators/email_validator.rb
+++ lib/validators/email_validator.rb
@@ -2,7 +2,7 @@ class EmailValidator < ActiveModel::EachValidator
   DEFAULT_ERROR_MESSAGE = "please enter a valid email address".freeze
 
   def validate_each(record, attribute, value)
-    unless value =~ /\A\S+@\S+\.\S+\z/i
+    unless value =~ /\A\S+@\S+\.\S+(?<!\.)\z/i
       record.errors[attribute] << (options[:message] || DEFAULT_ERROR_MESSAGE)
     end
   end
